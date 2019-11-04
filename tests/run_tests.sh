ef646aae2572e0bbd9f39fb2f62db691eb97e5a6
Leopold Talirz <leopold.talirz@gmail.com>
2019-11-04 16:21:19 +0100

    add travis test

---
+++ tests/run_tests.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+function check_changes
+{
+    #diff=`git diff-index --quiet HEAD --`
+    diff=`git diff .`
+
+    if [ -n "$diff" ]; then 
+        echo "### Changes detected:"
+        echo "$diff"
+        exit 1
+    fi
+}
+
+
+network -ha -res EDI.cssr
+check_changes
+network -ha -chan 1.5 EDI.cssr 
+check_changes
+network -ha -sa 1.2 1.2 2000 EDI.cssr 
+check_changes
+network -ha -vol 1.2 1.2 50000 EDI.cssr 
+check_changes
+network -ha -psd 1.2 1.2 50000 EDI.cssr
+check_changes
+
+network -ha -res history_1000.cssr 
+check_changes
+network -ha -sa 1.5 1.5 2000 history_1000.cssr 
+check_changes
+network -ha -vol 1.5 1.5 200000 history_1000.cssr 
+check_changes
+
+
