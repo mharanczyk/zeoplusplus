ce6f1ba846d3b8b87a0cca39cd6da1bc168a3c8a
Kevin Maik Jablonka <kevin.jablonka@epfl.ch>
2022-12-12 12:10:39 +0100

    print which test is running

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -3,10 +3,9 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
-function check_changes
-{
+function check_changes {
     #diff=`git diff-index --quiet HEAD --`
-    diff=`git diff --no-index $1 $2`
+    diff=$(git diff --no-index $1 $2)
     rm $1
     if [ -n "$diff" ]; then
         echo "### Changes detected:"
@@ -15,17 +14,27 @@ function check_changes
     fi
 }
 
+echo "Testing network -xyz ZIF-67_opt.cif"
 network -xyz ZIF-67_opt.cif
 check_changes ZIF-67_opt.xyz ZIF-67_opt_ref.xyz
 
+echo "Testing network -ha -res -allowAdjustCoordsAndCell EDI.cssr"
 network -ha -res -allowAdjustCoordsAndCell EDI.cssr
 check_changes EDI.res EDI_ref.res
+
+echo "Testing network -ha -chan 1.5 EDI.cssr"
 network -ha -chan 1.5 EDI.cssr
 check_changes EDI.chan EDI_ref.chan
+
+echo "Testing network -ha -sa 1.2 1.2 2000 EDI.cssr"
 network -ha -sa 1.2 1.2 2000 EDI.cssr
 check_changes EDI.sa EDI_ref.sa
+
+echo "network -ha -vol 1.2 1.2 50000 EDI.cssr"
 network -ha -vol 1.2 1.2 50000 EDI.cssr
 check_changes EDI.vol EDI_ref.vol
+
+echo "network -ha -psd 1.2 1.2 50000 EDI.cssr"
 network -ha -psd 1.2 1.2 50000 EDI.cssr
 check_changes EDI.psd_histo EDI_ref.psd_histo
 
